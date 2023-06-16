70315aa78d397bcc9cfc9bed2fdcd0f73b84b775
ChanHK <59449120+ChanHK@users.noreply.github.com>
2023-06-16 23:26:37 +0800

    added throttling

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from "express";
+import slowDown from "express-slow-down";
 import { connect } from "mongoose";
 import bodyParser from "body-parser";
 import api from "./api.js";
 
 const app = express();
 const port = 5000;
+
+const throttlingOptions = {
+  windowMs: 60 * 1000, // Throttling window: 1 minute
+  delayAfter: 100, // Start delaying responses after 100 requests
+  delayMs: 500, // Delay each response by 500 milliseconds
+};
 
 app.use(bodyParser.json());
 
@@ -17,6 +24,8 @@ connect(mongoUrl, {
     console.log(`MongoDB connected successfully`);
   })
   .catch((err) => console.log(err));
+
+app.use(slowDown(throttlingOptions));
 
 app.use("/shortify/api", api);
 
